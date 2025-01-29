ad3432372089ff71ae3ee356be2eb1ce5b04dee2
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2025-01-29 10:28:30 +0800

    添加修复程序未彻底退出问题TODO，添加exit()调用

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ if __name__ == '__main__':
     mainFrame = PureClipMainFrame(debug_log_level=3)
     mainFrame.Show(True)
     App.MainLoop()
+
+    # TODO: 解决程序没有彻底退出的问题
+    # 猜测是 App.MainLoop 主循环没有彻底结束
+    exit()
